004fe8295c4dd483e762b74a1c090b752a90c394
manuntag <manuntag@ualberta.ca>
2015-01-28 12:35:50 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by David Manuntag on 2015-01-28.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by David Manuntag on 2015-01-28.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
